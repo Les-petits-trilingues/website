c4e4c17849ef153bac8dae179bd9a96ec4fb7401
Élie Meignan <contact@eliepse.fr>
2021-01-11 21:34:59 +0100

    feat: add base styles and header content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ["./themes/lpt/index.php"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: ["./themes/lpt/index.php"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			black: "#000000",
+			white: "#ffffff",
+			gray: colors.gray,
+			beige: {
+				light: "#f4f1f1",
+			},
+			green: {
+				light: "#c1ebd5",
+				DEFAULT: "#0da84a",
+			},
+			orange: {
+				DEFAULT: "#a6a531",
+			}
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			//xl: '1280px',
+			//'2xl': '1536px',
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
--- themes/lpt/index.php
+++ themes/lpt/index.php
@@ -14,9 +14,33 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(['bg-beige-light']); ?>>
 
-<?= "LPT Theme"; ?>
+<?php include "components/header.php" ?>
+
+<main class="">
+	<div class="container m-auto">
+		<div class="mt-8 max-w-2xl m-auto text-center">
+			<img src="<?= asset('/images/logo-lpt.png') ?>"
+			     class="h-24 m-auto mb-8 sm:hidden"
+			     alt="Logo de LPT (Les Petits Trilingues)"
+			/>
+			<h1 class="text-5xl mb-6 font-bold">巴黎三语宝贝</h1>
+			<ul class="placesList text-sm mb-12">
+				<li class="inline-block">Belleville</li>
+				<li class="inline-block">Place d’Italie</li>
+				<li class="inline-block">Marais</li>
+				<li class="inline-block">Aubervilliers</li>
+			</ul>
+			<p class="px-4 mb-14">
+				LPT三语宝贝于2014年成立于法国巴黎，有20多位来自中法美三国的老师，我们已成长为法国知名的教育机构，获得逾千名学生和家长的信赖。
+			</p>
+			<a href="#" class="rounded-xl bg-orange text-white px-8 py-3">
+				开始注册
+			</a>
+		</div>
+	</div>
+</main>
 
 <?php wp_footer(); ?>
 <script src="<?= asset('main.js') ?>"></script>
