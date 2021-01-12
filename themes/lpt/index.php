0d12330bf84edda1a2fab6145121a891702b80a7
Élie Meignan <contact@eliepse.fr>
2021-01-12 23:57:24 +0100

    change: add the welcome image

--- themes/lpt/index.php
+++ themes/lpt/index.php
@@ -14,13 +14,13 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(['bg-beige-light']); ?>>
+<body <?php body_class(['bg-beige-light', 'font-sans']); ?>>
 
 <?php include "components/header.php" ?>
 
-<main class="">
-	<div class="container m-auto">
-		<div class="mt-8 max-w-2xl m-auto text-center">
+<main class="pb-24">
+	<section class="container m-auto">
+		<div class="mt-8 mb-16 max-w-2xl m-auto text-center">
 			<img src="<?= asset('/images/logo-lpt.png') ?>"
 			     class="h-24 m-auto mb-8 sm:hidden"
 			     alt="Logo de LPT (Les Petits Trilingues)"
@@ -35,11 +35,23 @@
 			<p class="px-4 mb-14">
 				LPT三语宝贝于2014年成立于法国巴黎，有20多位来自中法美三国的老师，我们已成长为法国知名的教育机构，获得逾千名学生和家长的信赖。
 			</p>
-			<a href="#" class="rounded-xl bg-orange text-white px-8 py-3">
+			<a href="#" class="inline-block rounded-xl bg-orange text-white px-8 py-3">
 				开始注册
 			</a>
 		</div>
-	</div>
+		<div class="mx-4 text-center">
+			<figure class=" inline-block">
+				<img src="<?= asset('images/group-students-happy.jpg') ?>"
+				     class="rounded-md mb-3"
+				     alt="Un groupe d'enfant souriant prend la pose"
+				/>
+				<figcaption class="text-sm text-center sm:text-left">
+					Visite du musée de l’imprimerie à Hanzhou, Chine
+				</figcaption>
+			</figure>
+		</div>
+	</section>
+	<section></section>
 </main>
 
 <?php wp_footer(); ?>
