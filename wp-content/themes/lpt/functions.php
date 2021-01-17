fe4feb3a35d4fc034dead7d2a3e8b4ab9978c4f3
Élie Meignan <contact@eliepse.fr>
2021-01-17 19:03:39 +0100

    refactor: new structure

--- wp-content/themes/lpt/functions.php
+++ wp-content/themes/lpt/functions.php
@@ -7,7 +7,7 @@ use App\Support\Environment;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Environment::boot(__DIR__ . '/..');
+Environment::boot(__DIR__ . '/../../..');
 $theme = Theme::getInstance();
 $theme->boot();
 
