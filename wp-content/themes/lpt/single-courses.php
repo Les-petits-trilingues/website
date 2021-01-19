bd3699bb4053786075a390682368a434abbe304a
Élie Meignan <contact@eliepse.fr>
2021-01-19 21:59:52 +0100

    fix: image position and size

--- wp-content/themes/lpt/single-courses.php
+++ wp-content/themes/lpt/single-courses.php
@@ -17,7 +17,7 @@ if (have_posts()):
 	<?php include "components/header.php" ?>
 	<main class="pb-24">
 		<header class="container px-4 mt-12 text-center relative sm:text-left sm:mx-auto">
-			<img src="<?= $course->headerImage ?>" alt="" class="m-auto mb-8 h-56 sm:absolute right-0" />
+			<img src="<?= $course->headerImage ?>" alt="" class="m-auto mb-8 h-56 sm:h-64 sm:absolute sm:-right-16" />
 			<h1 class="text-5xl font-bold mb-4 sm:text-6xl"><?= wp_title("") ?></h1>
 			<p class="mb-12 text-2xl"><?= $course->subtitle ?></p>
 			<ul class="sm:mx-0 course-caracteristicsList">
