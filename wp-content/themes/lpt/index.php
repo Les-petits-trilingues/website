f348024e112eb1de97d811c78e5bcf63a07ef729
Élie Meignan <contact@eliepse.fr>
2021-01-20 21:49:38 +0100

    change: add slanted background & fix spaces

--- wp-content/themes/lpt/index.php
+++ wp-content/themes/lpt/index.php
@@ -18,48 +18,50 @@ use App\Proxies\CoursePost;
 
 <?php include "components/header.php" ?>
 
-<main class="pb-24">
-	<section class="container m-auto">
-		<div class="mt-8 mb-16 max-w-2xl m-auto text-center sm:mt-32 sm:mb-36">
-			<img src="<?= asset('/images/logo-lpt.png') ?>"
-			     class="h-24 m-auto mb-8 sm:hidden"
-			     alt="Logo de LPT (Les Petits Trilingues)"
-			/>
-			<h1 class="text-5xl mb-6 font-bold">巴黎三语宝贝</h1>
-			<ul class="placesList text-sm mb-12">
-				<li class="inline-block">Belleville</li>
-				<li class="inline-block">Place d’Italie</li>
-				<li class="inline-block">Marais</li>
-				<li class="inline-block">Aubervilliers</li>
-			</ul>
-			<p class="px-4 mb-14">
-				LPT三语宝贝于2014年成立于法国巴黎，有20多位来自中法美三国的老师，我们已成长为法国知名的教育机构，获得逾千名学生和家长的信赖。
-			</p>
-			<a href="#" class="inline-block rounded-xl bg-orange text-white px-8 py-3">
-				开始注册
-			</a>
-		</div>
-		<div class="mx-4 text-center">
-			<figure class=" inline-block">
-				<img src="<?= asset('images/group-students-happy.jpg') ?>"
-				     class="rounded-md mb-3"
-				     alt="Un groupe d'enfant souriant prend la pose"
+<main>
+	<section class="slantedBackground pb-8">
+		<div class="container m-auto">
+			<div class="mt-8 mb-16 max-w-2xl m-auto text-center sm:mt-32 sm:mb-36">
+				<img src="<?= asset('/images/logo-lpt.png') ?>"
+				     class="h-24 m-auto mb-8 sm:hidden"
+				     alt="Logo de LPT (Les Petits Trilingues)"
 				/>
-				<figcaption class="text-sm text-center sm:text-left">
-					Visite du musée de l’imprimerie à Hanzhou, Chine
-				</figcaption>
-			</figure>
+				<h1 class="text-5xl mb-6 font-bold">巴黎三语宝贝</h1>
+				<ul class="placesList text-sm mb-12">
+					<li class="inline-block">Belleville</li>
+					<li class="inline-block">Place d’Italie</li>
+					<li class="inline-block">Marais</li>
+					<li class="inline-block">Aubervilliers</li>
+				</ul>
+				<p class="px-4 mb-14">
+					LPT三语宝贝于2014年成立于法国巴黎，有20多位来自中法美三国的老师，我们已成长为法国知名的教育机构，获得逾千名学生和家长的信赖。
+				</p>
+				<a href="#" class="inline-block rounded-xl bg-orange text-white px-8 py-3">
+					开始注册
+				</a>
+			</div>
+			<div class="mx-4 text-center">
+				<figure class=" inline-block">
+					<img src="<?= asset('images/group-students-happy.jpg') ?>"
+					     class="rounded-md mb-3"
+					     alt="Un groupe d'enfant souriant prend la pose"
+					/>
+					<figcaption class="text-sm text-center sm:text-left">
+						Visite du musée de l’imprimerie à Hanzhou, Chine
+					</figcaption>
+				</figure>
+			</div>
 		</div>
 	</section>
-	<section class="mx-4">
-		<h2 class="mt-16 sm:mt-32 mb-4 text-center font-bold text-4xl">课程介绍</h2>
+	<section class="pt-8 pb-24 px-4 bg-green-light">
+		<h2 class="sm:mt-32 mb-4 text-center font-bold text-4xl">课程介绍</h2>
 		<ul class="m-auto flex-wrap sm:max-w-4xl sm:flex">
 			<?php
 			$courses = CoursePost::fetchAll();
 			foreach ($courses as $index => $course) : ?>
 				<li
 					class="flex items-start flex-row-reverse flex-shrink-0 <?= $index < count($courses) - 1 ? "border-b" : "" ?>
-								 sm:border-0 border-gray-300 py-10 sm:block sm:w-1/3 sm:px-8"
+								 sm:border-0 border-gray-200 py-10 sm:block sm:w-1/3 sm:px-8"
 				>
 					<img
 						class="flex-none h-auto w-24 sm:max-h-20 sm:w-auto ml-6 sm:mx-auto sm:mb-6"
