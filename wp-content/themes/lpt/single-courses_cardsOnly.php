2d8ff52fef74f0bdf845eaa8dc5cc12b7ed87a10
Élie Meignan <contact@eliepse.fr>
2021-03-09 21:31:34 +0100

    fix: incorrect background for a course template

--- wp-content/themes/lpt/single-courses_cardsOnly.php
+++ wp-content/themes/lpt/single-courses_cardsOnly.php
@@ -18,9 +18,7 @@ if (have_posts()):
 	<!doctype html>
 	<html <?php language_attributes() ?>>
 	<?php include "components/head.php" ?>
-	<body <?php body_class(['bg-beige-light font-sans bg-no-repeat bg-center bg-top']); ?>
-		style="background-image: url(<?= asset("images/bg-1.svg") ?>); background-size: 100%;"
-	>
+	<body <?php body_class(['font-sans']); ?>>
 	<?php include "components/header.php" ?>
 	<main class="pb-24">
 
