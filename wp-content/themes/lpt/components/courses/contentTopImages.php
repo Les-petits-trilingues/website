de04f303fa158fed0c2444a6d4ea506a640c31f5
Élie Meignan <contact@eliepse.fr>
2021-03-09 22:00:55 +0100

    change: multiline for tip images & cleaner editor

--- wp-content/themes/lpt/components/courses/contentTopImages.php
+++ wp-content/themes/lpt/components/courses/contentTopImages.php
@@ -1,5 +1,6 @@
 <?php
 use App\Proxies\CoursePost;
+use App\Support\Arr;
 
 /**
  * @var stdClass $post
@@ -8,12 +9,16 @@ use App\Proxies\CoursePost;
 
 if ($course->contentTopImages): ?>
 	<section class="container mx-auto">
-		<ul class="flex justify-center items-stretch -mx-3 my-10">
-			<?php foreach ($course->contentTopImages as $image): ?>
-				<li class="mx-3">
-					<img src="<?= $image ?>" alt="" style="max-height: 12rem;"/>
-				</li>
+		<?php foreach (array_chunk(Arr::wrap($course->contentTopImages), 3) as $chunk): ?>
+
+			<ul class="flex justify-center items-stretch -mx-3 my-10">
+				<?php foreach ($chunk as $image): ?>
+					<li class="mx-3">
+						<img src="<?= $image ?>" alt="" style="max-height: 12rem;"/>
+					</li>
+				<?php endforeach; ?>
+
+			</ul>
 			<?php endforeach; ?>
-		</ul>
 	</section>
 <?php endif; ?>
