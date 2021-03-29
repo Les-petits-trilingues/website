31d26845c06c6dc57d835f8e4fe389452e62749c
Élie Meignan <contact@eliepse.fr>
2021-03-29 21:43:31 +0200

    fix: responsive column

--- wp-content/themes/lpt/single-courses.php
+++ wp-content/themes/lpt/single-courses.php
@@ -27,17 +27,19 @@ if (have_posts()):
 		<?php include "components/courses/contentTopImages.php" ?>
 
 		<?php foreach ($course->sections as $section) : ?>
-			<section class="container m-auto">
+			<section class="container m-auto px-3 sm:px-0">
 				<h2 class="text-4xl mt-16 mb-10"><?= $section["title"] ?></h2>
 				<p class="mb-6"><?= $section["description"] ?></p>
 				<?php foreach (array_chunk($section["cards"], 3) as $chunk) : ?>
-					<ul class="flex flex-wrap items-stretch -mx-3 mb-6">
+					<ul class="flex flex-wrap justify-center items-stretch -mx-3 mb-6">
 						<?php foreach ($chunk as $card) : ?>
-							<li class="bg-beige mx-3 px-8 py-6 flex-1 rounded-xl">
-								<img class="mb-6" src="<?= $card["images"][0] ?>" alt=""/>
-								<h3 class="text-xl"><?= $card[Localization::suffix("title")] ?></h3>
-								<p class="mb-4 text-lg"><?= $card[Localization::suffix("subtitle")] ?></p>
-								<p class="text-sm"><?= $card[Localization::suffix("description")] ?></p>
+							<li class="px-3 mb-6 w-full sm:w-1/3">
+								<div class="bg-beige h-full px-8 py-6 rounded-xl">
+									<img class="mb-6" src="<?= $card["images"][0] ?>" alt=""/>
+									<h3 class="text-xl"><?= $card[Localization::suffix("title")] ?></h3>
+									<p class="mb-4 text-lg"><?= $card[Localization::suffix("subtitle")] ?></p>
+									<p class="text-sm"><?= $card[Localization::suffix("description")] ?></p>
+								</div>
 							</li>
 						<?php endforeach; ?>
 					</ul>
