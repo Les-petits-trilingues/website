d240bbdc925990918360593dfbcaf59d4fdd3c66
Élie Meignan <em@globesailor.fr>
2021-04-01 11:00:35 +0200

    fix: invalid image url

--- wp-content/themes/lpt/components/courses/contentTopImages.php
+++ wp-content/themes/lpt/components/courses/contentTopImages.php
@@ -10,7 +10,7 @@ use App\Support\Arr;
 if ($course->contentTopImages): ?>
 	<section class="container mx-auto">
       <?php if(count(Arr::wrap($course->contentTopImages)) === 1): ?>
-        <img class="mx-auto mb-6" src="<?= $course->contentTopImages[0] ?>" alt=""/>
+        <img class="mx-auto mb-6" src="<?= Arr::wrap($course->contentTopImages)[0] ?>" alt=""/>
       <?php else: ?>
 		<?php foreach (array_chunk(Arr::wrap($course->contentTopImages), 3) as $chunk): ?>
 			<ul class="flex justify-center items-stretch -mx-3 my-10">
